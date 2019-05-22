e5b3ff6f6eb33482fca87cc2e13d5b53e06e307f
Bananamilk452 <bananamilk452@gmail.com>
2019-05-23 01:21:59 +0900

    commit completely

--- src/say/index.js
+++ src/say/index.js
@@ -3,7 +3,7 @@ const receive = {
     description: "당신이 말한 내용을 따라합니다.",
     parameters: ["말할 내용"],
     execute: function(message, bot, params) {
-        message.channel.send(params[1]);
+        message.channel.send(message.content.substring(5));
     }
 };
 
